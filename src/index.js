6c2cc2bdf48a2d9d085771784d642aa800dc2232
ot87 <oleksandr.tsyrkul@gmail.com>
2021-04-25 18:42:21 +0300

    Add Material-UI and PropTypes. Use hot reload for components.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-);
+const renderApp = () => {
+  render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./App', renderApp);
+}
+
+renderApp();
